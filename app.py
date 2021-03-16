5e6b213611d069fecb79bf834022ca6cfad5b635
Guilherme Roncato <guilhermeroncato@gmail.com>
2021-03-16 17:43:39 -0300

    Criando Redshift

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from data_platform.dms.stack import DmsStack
 from data_platform.kinesis.stack import KinesisStack
 from data_platform.glue_catalog.stack import GlueCatalogStack
 from data_platform.athena.stack import AthenaStack
+from data_platform.redshift.stack import RedshiftStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
@@ -13,4 +14,5 @@ dms = DmsStack(app, common_stack = common_stack, data_lake_raw_bucket = data_lak
 kinesis = KinesisStack(app, data_lake_raw_bucket = data_lake.data_lake_raw_bucket)
 glue_catalog = GlueCatalogStack(app, raw_data_lake_bucket = data_lake.data_lake_raw_bucket, processed_data_lake_bucket = data_lake.data_lake_processed_bucket)
 athena = AthenaStack(app)
+redshift = RedshiftStack(app, data_lake_raw = data_lake.data_lake_raw_bucket, data_lake_processed = data_lake.data_lake_processed_bucket, common_stack = common_stack)
 app.synth()
